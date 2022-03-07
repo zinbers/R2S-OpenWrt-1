563627303283d6e4fdf484aadfd56d6537c5275d
zinbers <luckyboyzb@gmail.com>
2022-03-07 16:48:27 +0800

    Update SSRPlus.sh

--- PATCH/app/SSRPlus.sh
+++ PATCH/app/SSRPlus.sh
@@ -57,4 +57,10 @@ pushd package/lean/luci-app-ssr-plus
   done
 popd
 
+# UPX 可执行软件压缩
+sed -i '/patchelf pkgconf/i\tools-y += ucl upx' ./tools/Makefile
+sed -i '\/autoconf\/compile :=/i\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/upx tools/upx
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/ucl tools/ucl
+
 exit 0
